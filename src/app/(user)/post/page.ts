8730f5051ee133b1d0e202aaf774f5fe3f16a38b
Chabiao44 <ckhemaramylol@gmail.com>
2025-09-26 23:26:30 +0700

    Initial commit 2

--- src/app/(user)/post/page.ts
+++ src/app/(user)/post/page.ts
@@ -1,37 +1,22 @@
-    import axios from "axios"; 
+import axios from "axios";
 
-    
+type Post = { id: number; title: string };
 
-    type Post = { id: number; title: string }; 
+export default async function PostsPage() {
+  // ✅ ดึงข้อมูลด้วย axios
+  const res = await axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts");
+  const posts = res.data;
 
-    
-
-    export default async function PostsPage() { 
-
-    const res = await axios.get<Post[]>("https://jsonplaceholder.typicode.com/posts"); 
-
-    const posts = res.data; 
-
-    
-
-    return ( 
-
-        <div> 
-
-        <h1>Posts (Axios)</h1> 
-
-        <ul> 
-
-            {posts.map((p,index)) => ( 
-
-            <li key={p.id}>{index + 1}. {p.title}</li> 
-
-            ))} 
-
-        </ul> 
-
-        </div> 
-
-    ); 
-
-    } 
+  return (
+    <div>
+      <h1>Posts (Axios)</h1>
+      <ul>
+        {posts.map((p, index) => (
+          <li key={p.id}>
+            {index + 1}. {p.title}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
